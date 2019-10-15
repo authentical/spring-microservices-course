a5d1d2ea37a941911ed47066b8c13f9f44abca14
authentical <cvspencer@gmail.com>
2019-10-15 15:40:48 -0300

    Added logging for all methods. Created createUser, updateUser, getUser, deleteUser

--- mobile-app-ws/src/main/java/com/potatospy/app/ws/ui/controllers/UserController.java
+++ mobile-app-ws/src/main/java/com/potatospy/app/ws/ui/controllers/UserController.java
@@ -1,18 +1,29 @@
 package com.potatospy.app.ws.ui.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.potatospy.app.ws.ui.model.request.UpdateUserDetailsRequestModel;
+import com.potatospy.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.potatospy.app.ws.ui.model.response.UserRest;
 
 
@@ -20,16 +31,23 @@ import com.potatospy.app.ws.ui.model.response.UserRest;
 @RequestMapping("users") // local 8080 /users
 public class UserController {
 	
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
     
     @PostConstruct
     public void init() {
-    	logger.debug("Logger is initialized for " + this.getClass());
+		log.info("Logger is initialized for " + this.getClass());
     }
+    
+    
+    Map<String, UserRest> usersMap = new HashMap<String, UserRest>();
+    
     
 	
 	// Get all users and display page and optionally limit per page
-	@GetMapping()						// If ReqParam param required=false, defaultValue IS required because ReqParam int is primitive and can't be converted to null which would be required if no defaultValue is set
+	@GetMapping(
+			produces = {MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE}
+			)// If ReqParam param required=false, defaultValue IS required because ReqParam int is primitive and can't be converted to null which would be required if no defaultValue is set
 	public UserRest getUsers(	
 			@RequestParam(value="page", defaultValue="1", required=false) int page,	// Requestparam params other than boolean must be strings only	
 			@RequestParam(value="limit", defaultValue="50", required=false) int limit,
@@ -37,39 +55,149 @@ public class UserController {
 			) {
 		
 		
-		logger.debug("got all users. Page " + page + " with limit per page " + limit);
+		log.info("got all users. Page " + page + " with limit per page " + limit);
 		
 		return new UserRest();
 	}
 	
 	
 	
-	@GetMapping(path="/{userId}")	// url/RequestMapping/PathVariable
-	public String getUser(@PathVariable String userId) {
+	@GetMapping(
+			path = "/{userId}", // url/RequestMapping/PathVariable
+			produces = { MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE }
+			) 
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
 		
-		return "got user " + userId;
+		log.info("got user " + userId);
+		
+		
+		String firstName = null;
+		//
+		//
+		int firstNameLength = firstName.length();
+		
+		
+		if(usersMap.containsKey(userId)) { 
+			return new ResponseEntity<UserRest>(usersMap.get(userId), HttpStatus.OK); 	
+		}
+
+		return new ResponseEntity<UserRest>(HttpStatus.NO_CONTENT);
 	}
 	
 	
 	
-	@PostMapping
-	public String createUser() {
+	@PostMapping(
+			produces = { 
+					MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE 
+					}, 
+			consumes = {
+					MediaType.APPLICATION_JSON_VALUE, 
+					MediaType.APPLICATION_XML_VALUE 
+					})
+	public ResponseEntity<UserRest> createUser(
+			@Valid @RequestBody UserDetailsRequestModel userDetails
+			) {
 		
-		return "created user";
+		log.info("################>" + this.getClass().getMethods().toString() + " needs it's log info set");
+		
+		UserRest returnValue = new UserRest();
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		returnValue.setEmail(userDetails.getEmail());
+		
+		String userId	=	UUID.randomUUID().toString();
+		returnValue.setUserId(userId);
+		
+		if(usersMap==null) usersMap = new HashMap<>();
+		usersMap.put(userId, returnValue);
+		
+		
+		return new ResponseEntity<UserRest>(returnValue, HttpStatus.OK);
 	}
 	
 	
 	
-	@PutMapping
-	public String updateUser() {
+	@PutMapping(
+			path = "/{userId}",
+			produces = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+			}, 
+	consumes = {
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+			})
+	public UserRest updateUser(	// Todo Dont know why we not returning RequestEntity here
+			@Valid @RequestBody UpdateUserDetailsRequestModel updateUserDetailsRequestModel,
+			@PathVariable String userId
+			) {
 		
-		return "updated user";
+		
+		log.info("################>" + this.getClass().getMethods().toString() + " needs it's log info set");
+
+		
+		// Transfer updateUserDetailsRequestModel to storedUserRest
+		UserRest storedUserRest = usersMap.get(userId);
+		storedUserRest.setFirstName(updateUserDetailsRequestModel.getFirstName());
+		storedUserRest.setLastName(updateUserDetailsRequestModel.getLastName());
+		
+		
+		// Transfer storedUserRest to usersMap
+		if(usersMap.containsKey(userId)) {
+			usersMap.put(userId, storedUserRest);
+		}
+		
+		
+		return usersMap.get(userId);
 	}
 	
 	
-	@DeleteMapping
-	public String deleteUser() {
+	@DeleteMapping(
+			path = "/{userId}",
+			produces = { 
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+			}, 
+	consumes = {
+			MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE 
+			})
+	public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
 		
-		return "deleted user";
+		
+		log.info("################>" + this.getClass().getMethods().toString() + " needs it's log info set");
+
+		
+		if(usersMap.containsKey(userId)) {
+			
+			
+			usersMap.remove(userId);
+		}
+		
+		
+		
+		// return new ResponseEntity.noContent().build();
+		return  new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

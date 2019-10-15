1d077b5b3d7d16076765cf2b491d0f35aaf84f22
authentical <cvspencer@gmail.com>
2019-10-15 20:15:14 -0300

    Configured to send ErrorMessage in response instead of complete Exception message

--- mobile-app-ws/src/main/java/com/potatospy/app/ws/exceptions/AppExceptionsHandler.java
+++ mobile-app-ws/src/main/java/com/potatospy/app/ws/exceptions/AppExceptionsHandler.java
@@ -1,11 +1,15 @@
 package com.potatospy.app.ws.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import com.potatospy.app.ws.ui.model.response.ErrorMessage;
 
 
 // ControllerAdvice is specialized @Component but it is used to apply to all Controllers globally
@@ -15,6 +19,14 @@ public class AppExceptionsHandler {
 	
 	@ExceptionHandler(value = {Exception.class})
 	public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
+		
+		
+		// Create an error message with timestamp NOW and 
+		// (the localized exception message, OR if that does not exist, the exception to string)
+		ErrorMessage errorMessage = new ErrorMessage(
+				new Date(), 
+				ex.getLocalizedMessage().equals(null)? ex.toString():ex.getLocalizedMessage()
+				);
 		
 		return new  ResponseEntity<>(ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}

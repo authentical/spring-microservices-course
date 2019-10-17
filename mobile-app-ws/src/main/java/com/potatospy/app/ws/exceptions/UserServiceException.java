9a64e39209db118d9648f20be59b669d613d8e1b
authentical <cvspencer@gmail.com>
2019-10-17 16:41:49 -0300

    Added UserServiceException

---
+++ mobile-app-ws/src/main/java/com/potatospy/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,20 @@
+package com.potatospy.app.ws.exceptions;
+
+
+
+
+public class UserServiceException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -681991492884005033L;
+	
+	
+	public UserServiceException(String message) {
+		
+		super(message);
+	}
+	
+
+}

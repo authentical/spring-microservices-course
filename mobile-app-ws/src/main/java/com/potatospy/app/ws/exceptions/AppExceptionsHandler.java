792ab5d674445b53ea0dcc366ddbcdb12c295d17
authentical <cvspencer@gmail.com>
2019-10-17 16:27:07 -0300

    Added UserServiceException.class ExceptionHandler
    This allows response to client as a friendly String

--- mobile-app-ws/src/main/java/com/potatospy/app/ws/exceptions/AppExceptionsHandler.java
+++ mobile-app-ws/src/main/java/com/potatospy/app/ws/exceptions/AppExceptionsHandler.java
@@ -12,14 +12,18 @@ import org.springframework.web.context.request.WebRequest;
 import com.potatospy.app.ws.ui.model.response.ErrorMessage;
 
 
-// ControllerAdvice is specialized @Component but it is used to apply to all Controllers globally
+// In combination with other Error Message and custom Exception(extended Runtime) classes,
+// This class is used to send custom exception messages in web response body instead
 
+
+
+// ControllerAdvice is specialized @Component but it is used to apply to all Controllers globally
 @ControllerAdvice
 public class AppExceptionsHandler {
 	
+	
 	@ExceptionHandler(value = {Exception.class})
 	public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
-		
 		
 		
 		// Create an error message with timestamp NOW and 
@@ -32,6 +36,7 @@ public class AppExceptionsHandler {
 				new Date(), 
 				errorDescription
 				);
+		
 		
 		return new  ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
@@ -51,10 +56,28 @@ public class AppExceptionsHandler {
 						errorDescription
 						);
 				
+				
 				return new  ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-		
 	}
 	
+	
+	@ExceptionHandler(value= {UserServiceException.class})
+	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
+		
+				// Create an error message with timestamp NOW and 
+				// (the localized exception message, OR if that does not exist, the exception tostring)
+				
+				String errorDescription = ex.getLocalizedMessage();
+				if(errorDescription==null) errorDescription = ex.toString();
+				
+				ErrorMessage errorMessage = new ErrorMessage(
+						new Date(), 
+						errorDescription
+						);
+				
+				
+				return new  ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 	
 
 }

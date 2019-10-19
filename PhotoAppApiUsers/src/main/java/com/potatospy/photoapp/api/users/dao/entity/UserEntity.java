aedbd87807c7576688ae1146110315c9c7124f1c
authentical <cvspencer@gmail.com>
2019-10-19 18:27:43 -0300

    Added UserEntity to initialize H2 DB
    H2 console test failed to JDBC:h2:mem:testdb
    
    Fixed by adding JPA dependency and spring.jpa.show-sql=true to app props

---
+++ PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/dao/entity/UserEntity.java
@@ -0,0 +1,15 @@
+package com.potatospy.photoapp.api.users.dao.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class UserEntity {
+	
+	@Id
+	private String id;
+	
+
+}

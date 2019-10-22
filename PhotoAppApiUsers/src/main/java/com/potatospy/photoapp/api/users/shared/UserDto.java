ad5cc4ef84c901fefdc46db89ae85a5ec6bca5be
authentical <cvspencer@gmail.com>
2019-10-21 23:07:06 -0300

    Added Lombok get/set

--- PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/shared/UserDto.java
+++ PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/shared/UserDto.java
@@ -11,16 +11,23 @@ import lombok.Setter;
 public class UserDto {
 	
 
+	@Getter
+	@Setter
 	private String firstName;
-
+	@Getter
+	@Setter
 	private String lastName;
-
+	@Getter
+	@Setter
 	private String password;
-
+	@Getter
+	@Setter
 	private String email;
-	
+	@Getter
+	@Setter
 	private String encryptedPassword;
-
+	@Getter
+	@Setter
 	private String userId;
 
 }

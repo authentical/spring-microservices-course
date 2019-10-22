ae3aaf5ba0c5236d2c94d9a394de89f617dae0b9
authentical <cvspencer@gmail.com>
2019-10-21 23:06:56 -0300

    Service can now take user from controller and save to DB

--- PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/service/UsersServiceImpl.java
@@ -1,17 +1,92 @@
 package com.potatospy.photoapp.api.users.service;
 
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.potatospy.photoapp.api.users.dao.entity.UserEntity;
+import com.potatospy.photoapp.api.users.dao.repo.UsersRepository;
 import com.potatospy.photoapp.api.users.shared.UserDto;
 
-@Service
+
+@Service // Todo add interface and implement @Service from there
 public class UsersServiceImpl {
 
+	
+	@Autowired
+	UsersRepository usersRepository;
+	
+	
+	
+	
+	
+	public UsersServiceImpl(UsersRepository usersRepository) {
+		
+		this.usersRepository = usersRepository;
+	}
+	
+	
+	
 	
 	
 	public UserDto createUser(UserDto userDetails) {
 		
 		
-		return new UserDto();
+		
+		userDetails.setUserId(UUID.randomUUID().toString());
+		
+		
+		
+		ModelMapper modelMapper = new ModelMapper();
+		// MatchingStrategies.STRICT : ModelMapper does a lot of magic and if for some reason
+		// it gets confused about member names between classes, it'll blow up. So use STRICT in this example..
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserEntity userEntityToRepository = modelMapper.map(userDetails, UserEntity.class);
+		userEntityToRepository.setEncryptedPassword("JUNK");
+		
+		
+		usersRepository.save(userEntityToRepository);
+		
+		
+		return null;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

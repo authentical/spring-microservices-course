6982547863973d6b0399dab04ef442feb80978d6
authentical <cvspencer@gmail.com>
2019-10-21 23:06:14 -0300

    Blank Update

--- PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -1,8 +1,12 @@
 package com.potatospy.photoapp.api.users;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.potatospy.photoapp.api.users.shared.UserDto;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +15,6 @@ public class PhotoAppApiUsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
 	}
+	
 
 }

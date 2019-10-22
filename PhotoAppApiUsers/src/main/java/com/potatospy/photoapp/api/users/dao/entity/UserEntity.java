0c6ea1da2dbc0f2e27b7d4ebe5cabd3c7df98aa0
authentical <cvspencer@gmail.com>
2019-10-21 23:05:14 -0300

    Added Users table definition

--- PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/dao/entity/UserEntity.java
+++ PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/dao/entity/UserEntity.java
@@ -1,15 +1,57 @@
 package com.potatospy.photoapp.api.users.dao.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 
 
 @Entity
-public class UserEntity {
+@Table(name="users")
+public class UserEntity implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1456019248248L;
+	
+	
 	
 	@Id
-	private String id;
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false, length=50)
+	@Getter
+	@Setter
+	private String firstName;
+	@Column(nullable=false, length=50)
+	@Getter
+	@Setter
+	private String lastName;
+//	@Getter
+//	@Setter
+//	private String password; // Obv dont save unencrypted
+	@Column(nullable=false, length=120, unique=true)
+	@Getter
+	@Setter
+	private String email;
+	@Column(nullable=false, unique=true)
+	@Getter
+	@Setter
+	private String encryptedPassword;
+	@Column(nullable=false, unique=true)
+	@Getter
+	@Setter
+	private String userId;
+	
 	
 
 }

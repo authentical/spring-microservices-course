f88ecce43a4de8b237504776858d0c5412011e36
authentical <cvspencer@gmail.com>
2019-10-22 23:50:36 -0300

    ADded Bean BCryptPasswordEncoder

--- PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/potatospy/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.potatospy.photoapp.api.users.shared.UserDto;
 
@@ -17,4 +18,11 @@ public class PhotoAppApiUsersApplication {
 	}
 	
 
+	
+	@Bean	// This is injected into UsersService
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		
+		
+		return 
+	}
 }
